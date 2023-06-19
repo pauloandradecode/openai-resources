fc1c09aa8dfe474cfaf59287c86d5811247eff04
Paulo César Andrade González <PauloAndrade@GDLMB005545.local>
2023-06-19 16:26:01 -0600

    Example embedding with redis

--- 02_redis_embedding_example.py
+++ 02_redis_embedding_example.py
@@ -13,7 +13,10 @@ model = 'text-embedding-ada-002'
 # creamos un cliente de redis
 class RedisClient:
     def __init__(self, host='localhost', port=6379):
-        self.client = redis.Redis(host=host, port=port)
+        try:
+            self.client = redis.Redis(host=host, port=port)
+        except ConnectionError:
+            print("Connection Error")
 
     def set(self, key, value):
         try:
@@ -61,14 +64,14 @@ class RedisClient:
             print("Connection Error")
 
 
+# create a redis client
+redis_client = RedisClient()
+
 # retrieve the embedding from the redis cache
 # if it doesn't exist, create it and store it in the cache
 # then return it
 
 def get_embedding(text):
-    # create a redis client
-    redis_client = RedisClient()
-
     # redis_client.delete(text)
 
     # if the embedding doesn't exist in the cache
@@ -93,8 +96,82 @@ def get_embedding(text):
     return embeddings
 
 
+# calculate the cosine similarity between two embeddings
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+# calculate the similarity between two texts
+def similarity(text1, text2):
+    # get the embeddings for the texts
+    embeddings1 = get_embedding(text1)
+    embeddings2 = get_embedding(text2)
+
+    # calculate the cosine similarity between the embeddings
+    return cosine_similarity(embeddings1, embeddings2)
+
+
+# calculate the similarity between a text and a list of texts
+def similarity_list(text, text_list):
+    # get the embedding for the text
+    embeddings = get_embedding(text)
+    
+    # calculate the cosine similarity between the embedding and each text in the list
+    return [cosine_similarity(embeddings, get_embedding(text2)) for text2 in text_list]
+
+
+# Calculate the similarity between a text and a list of texts and return the most similar text
+def most_similar(text, text_list):
+    
+    # get the list of similarities
+    similarities = similarity_list(text, text_list)
+    
+    # return the text with the highest similarity
+    return text_list[np.argmax(similarities)]
+
+
+# calculate the similarity between a text and a list of texts
+# and return the most similar text and its similarity
+def most_similar_with_similarity(text, text_list):
+    # get the list of similarities
+    similarities = similarity_list(text, text_list)
+
+    # get the index of the highest similarity
+    index = np.argmax(similarities)
+
+    # return the text with the highest similarity and its similarity
+    return text_list[index], similarities[index]
+
+
+# calculate the similarity between a text and a list of texts
+# and return the most similar text and its similarity
+# if the similarity is below a threshold, return None
+def most_similar_with_similarity_threshold(text, text_list, threshold):
+    # get the list of similarities
+    similarities = similarity_list(text, text_list)
+
+    # get the index of the highest similarity
+    index = np.argmax(similarities)
+
+    # if the similarity is below the threshold
+    if similarities[index] < threshold:
+        # return None
+        return None, None
+    # otherwise
+    else:
+        # return the text with the highest similarity and its similarity
+        return text_list[index], similarities[index]
+
+
 # Ejemplo de funcionamiento
-embeddings = get_embedding("This is a test")
-print(embeddings)
+# embeddings = get_embedding("This is a test")
+# print(embeddings)
 
 # openai.Engine.list() 
+
+# BÚSQUEDAS INTELIGENTES CON OPENAI
+text_list = ['Los felinos dicen', 'Los caninos dicen', 'Los bovinos dicen', 'Tengo un terreno en ayacucho', 'Tengo una casa en Piura']
+
+text = 'soy rico'
+
+print(f'El texto {text} es similar a: {most_similar(text, text_list)}')
